f156f98aa27d89e8a533d9fa5f03f853f7ed31c2
Alex Dean <alex@crackpot.org>
2013-02-04 10:52:41 -0600

    add rdoc/yard documentation
    mark buffer_clear_pending private since it should be.

--- lib/stud/buffer.rb
+++ lib/stud/buffer.rb
@@ -1,6 +1,79 @@
 module Stud
+
+  # Implements a generic framework for accepting events which are later flushed
+  # in batches. Flushing occurrs whenever +:max_items+ or +:max_interval+ (seconds)
+  # has been reached.
+  #
+  # Including class must implement +flush+, which will be called with all accumulated
+  # items either when the output buffer fills (+:max_items+) or when a fixed amount
+  # of time (+:max_interval+) passes.
+  #
+  # == batch_receive and flush
+  # General receive/flush can be implemented in one of two ways.
+  #
+  # === batch_receive(event) / flush(events)
+  # +flush+ will receive an array of events which were passed to +buffer_receive+.
+  #
+  #   batch_receive('one')
+  #   batch_receive('two')
+  #
+  # will cause a flush invocation like
+  #
+  #   flush(['one', 'two'])
+  #
+  # === batch_receive(event, group) / flush(events, group)
+  # flush() will receive an array of events, plus a grouping key.
+  #
+  #   batch_receive('one',   :server => 'a')
+  #   batch_receive('two',   :server => 'b')
+  #   batch_receive('three', :server => 'a')
+  #   batch_receive('four',  :server => 'b')
+  #
+  # will result in the following flush calls
+  #
+  #   flush(['one', 'three'], {:server => 'a'})
+  #   flush(['two', 'four'],  {:server => 'b'})
+  #
+  # Grouping keys can be anything which are valid Hash keys. (They don't have to
+  # be hashes themselves.) Strings or Fixnums work fine. Use anything which you'd
+  # like to receive in your +flush+ method to help enable different handling for
+  # various groups of events.
+  #
+  # == on_flush_error
+  # Including class may implement +on_flush_error+, which will be called with an
+  # Exception instance whenever buffer_flush encounters an error.
+  #
+  # * +buffer_flush+ will automatically re-try failed flushes, so +on_flush_error+
+  #   should not try to implement retry behavior.
+  # * Exceptions occurring within +on_flush_error+ are not handled by
+  #   +buffer_flush+.
+  #
+  # == on_full_buffer_receive
+  # Including class may implement +on_full_buffer_receive+, which will be called
+  # whenever +buffer_receive+ is called while the buffer is full.
+  #
+  # +on_full_buffer_receive+ will receive a Hash like <code>{:pending => 30,
+  # :outgoing => 20}</code> which describes the internal state of the module at
+  # the moment.
+  #
+  # == final flush
+  # Including class should call <code>buffer_flush(:final => true)</code> during a teardown/
+  # shutdown routine (after the last call to buffer_receive) to ensure that all
+  # accumulated messages are flushed.
   module Buffer
 
+    # Initialize the buffer.
+    #
+    # Call directly from your constructor if you wish to set some non-default
+    # options. Otherwise buffer_initialize will be called automatically during the
+    # first buffer_receive call.
+    #
+    # Options:
+    # * :max_items, Max number of items to buffer before flushing. Default 50.
+    # * :max_interval, Max number of seconds to wait between flushes. Default 5.
+    # * :logger, A logger to write log messages to. No default. Optional.
+    #
+    # @param [Hash] options
     def buffer_initialize(options={})
       if ! self.class.method_defined?(:flush)
         raise ArgumentError, "Any class including Stud::Buffer must define a flush() method."
@@ -42,18 +115,27 @@ module Stud
       buffer_clear_pending
     end
 
-    def buffer_clear_pending
-      @buffer_state[:pending_items] = Hash.new { |h, k| h[k] = [] }
-      @buffer_state[:pending_count] = 0
-    end
-
+    # Determine if +:max_items+ has been reached.
+    #
+    # buffer_receive calls will block while <code>buffer_full? == true</code>.
+    #
+    # @return [bool] Is the buffer full?
     def buffer_full?
       @buffer_state[:pending_count] + @buffer_state[:outgoing_count] >= @buffer_config[:max_items]
     end
 
-    # save an event for later delivery
-    # events are grouped by the (optional) group parameter you provide
-    # groups of events, plus the group name, are passed to your batch_flush() method
+    # Save an event for later delivery
+    #
+    # Events are grouped by the (optional) group parameter you provide.
+    # Groups of events, plus the group name, are later passed to +flush+.
+    #
+    # This call will block if +:max_items+ has been reached.
+    #
+    # @see Stud::Buffer The overview has more information on grouping and flushing.
+    #
+    # @param event An item to buffer for flushing later.
+    # @param group Optional grouping key. All events with the same key will be
+    #              passed to +flush+ together, along with the grouping key itself.
     def buffer_receive(event, group=nil)
       buffer_initialize if ! @buffer_state
 
@@ -74,10 +156,24 @@ module Stud
       buffer_flush
     end
 
-    # try to flush events
-    # return immediately if we fail to get lock (another flush is in progress)
-    #   unless called with :final => true. In that case, wait for a lock.
-    # returns the number of items flushed successfully
+    # Try to flush events.
+    #
+    # Returns immediately if flushing is not necessary/possible at the moment:
+    # * :max_items have not been accumulated
+    # * :max_interval seconds have not elapased since the last flush
+    # * another flush is in progress
+    #
+    # <code>buffer_flush(:force => true)</code> will cause a flush to occur even
+    # if +:max_items+ or +:max_interval+ have not been reached. A forced flush
+    # will still return immediately (without flushing) if another flush is
+    # currently in progress.
+    #
+    # <code>buffer_flush(:final => true)</code> is identical to <code>buffer_flush(:force => true)</code>,
+    # except that if another flush is already in progress, <code>buffer_flush(:final => true)</code>
+    # will block/wait for the other flush to finish before proceeding.
+    #
+    # @param [Hash] options Optional. May be <code>{:force => true}</code> or <code>{:final => true}</code>.
+    # @return [Fixnum] The number of items successfully passed to +flush+.
     def buffer_flush(options={})
       force = options[:force] || options[:final]
       final = options[:final]
@@ -88,6 +184,8 @@ module Stud
       elsif ! @buffer_state[:flush_mutex].try_lock # failed to get lock, another flush already in progress
         return 0
       end
+
+      items_flushed = 0
 
       begin
         time_since_last_flush = Time.now.to_i - @buffer_state[:last_flush]
@@ -112,7 +210,6 @@ module Stud
           :final => final
         ) if @buffer_config[:logger]
 
-        items_flushed = 0
         @buffer_state[:outgoing_items].each do |group, events|
           begin
             if group.nil?
@@ -150,5 +247,12 @@ module Stud
 
       items_flushed
     end
+  end
+
+  private
+
+  def buffer_clear_pending
+    @buffer_state[:pending_items] = Hash.new { |h, k| h[k] = [] }
+    @buffer_state[:pending_count] = 0
   end
 end
