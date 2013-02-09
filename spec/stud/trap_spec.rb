a5a94039ee295b04f6d9c5e69de270683aa92bf8
Jordan Sissel <jls@semicomplete.com>
2013-02-09 15:43:12 -0800

    - signals are handled by a separate thread in Ruby/JRuby, so use a Queue   to track handler calls.

--- spec/stud/trap_spec.rb
+++ spec/stud/trap_spec.rb
@@ -1,36 +1,50 @@
 require "stud/trap"
+require "stud/try"
 require "spec_env" # from the top level spec/ directory
-
-class FakeFailure < StandardError; end
+require "timeout"
 
 describe "Stud#trap" do
+
   it "should call multiple traps for a single signal" do
-    count = 0
-    Stud.trap("USR1") { puts "USR1"; count += 1 }
-    Stud.trap("USR1") { puts "USR2"; count += 2 }
-    Stud.trap("USR1") { puts "USR3"; count += 3 }
-    Process.kill("USR1", Process.pid)
-    sleep(0.5)
-    insist { count } == 6
+    queue = Queue.new
+    Stud.trap("USR2") { queue << 1 }
+    Stud.trap("USR2") { queue << 2 }
+    Stud.trap("USR2") { queue << 3 }
+    Process.kill("USR2", Process.pid)
+
+    Stud.try(10.times) do
+      insist { queue.size } == 3
+    end
+
+    insist { queue.pop } == 1
+    insist { queue.pop } == 2
+    insist { queue.pop } == 3
   end
 
   it "should keep any original traps set with Kernel#trap" do
     hupped = false
     studded = false
-    Kernel.trap("HUP") { hupped = true }
-    Stud.trap("HUP") { studded = true }
-    Process.kill("HUP", Process.pid)
-    insist { hupped } == true
-    insist { studded } == true
-  end
 
-  it "should override" do
-    hupped = false
-    studded = false
-    Kernel.trap("HUP") { hupped = true }
-    Stud.trap("HUP") { studded = true }
+    queue = Queue.new
+    # Set a standard signal using the ruby stdlib method
+    Kernel.trap("HUP") { queue << :kernel }
+
+    # This should still keep the previous trap.
+    Stud.trap("HUP") { queue << :stud }
+
+    # Send SIGHUP
     Process.kill("HUP", Process.pid)
-    insist { hupped } == true
-    insist { studded } == true
+
+    # Wait for both signal handlers to get called.
+    Stud.try(10.times) do
+      insist { queue.size } == 2
+    end
+
+    # Kernel handler should get called first since it was 
+    # there first.
+    insist { queue.pop } == :kernel
+
+    # Our stud.trap should get called second
+    insist { queue.pop } == :stud
   end
 end
